2dfb53a7904547cff5414cba983a358233f54693
yevheniigoman <zhgoman@gmail.com>
2025-12-04 15:00:38 +0200

    added endpoint for reading specific aircraft

--- src/routers/aircrafts.py
+++ src/routers/aircrafts.py
@@ -2,8 +2,8 @@ from dependencies import get_session
 from models import Aircraft
 from schemas.aircrafts import AircraftRead
 from sqlalchemy import select
-from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session, exc
+from fastapi import APIRouter, Depends, HTTPException
 
 
 router = APIRouter(prefix="/aircrafts")
@@ -14,3 +14,14 @@ def read_aircrafts(session: Session = Depends(get_session)):
     stmt = select(Aircraft)
     result = session.execute(stmt)
     return result.scalars().all()
+
+
+@router.get("/{tail_number}", tags=["aircrafts"], response_model=AircraftRead)
+def read_aircraft(tail_number: str, session: Session = Depends(get_session)):
+    stmt = select(Aircraft).where(Aircraft.tail_number == tail_number)
+    result = session.execute(stmt)
+    try:
+        aircraft = result.scalars().one()
+    except exc.NoResultFound:
+        raise HTTPException(status_code=404, detail="Aircraft not found")
+    return aircraft
